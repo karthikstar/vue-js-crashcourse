20ac2a15afc7331a462c0a7c97c0a504dfe5327b
elkarthi2000@gmail.com <elkarthi2000@gmail.com>
2021-03-22 02:56:28 +0800

    task tracker v1

---
+++ task-tracker/vue-crash-2021/src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button text = "Add Task" color = "green" />
+        <!-- <Button text = "Update Task" color = "blue" /> -->
+
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+export default {
+    name: 'Header',
+    // props: ['title'] - 1 way
+    // alternate way define props as a object and give title a specific type
+    props: {
+        title: String
+    },
+
+    components: {
+        Button
+    }
+
+    // props: {
+    //     title : {
+    //         type : String,
+    //         default: 'Hello World'
+    //         // this will show if no value  passed in for title prop
+    //     }
+    // }
+}
+</script>
+
+<style scoped>
+/* scoped means everything thats here is  only gonna be for this component*/
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
---
+++ task-tracker/vue-crash-2021/src/components/Tasks.vue
@@ -0,0 +1,28 @@
+<template>
+    <div :key="task.id" v-for = "task in tasks">
+        <!-- v for directive, meant for arrays -->
+        <!-- <h3>{{task.text}}</h3> -->
+        <Task :task = "task" />
+    </div>
+
+</template>
+
+<script>
+import Task from './Task'
+
+export default {
+    name: 'Tasks',
+    props: {
+        tasks: Array,
+    },
+
+    // we are gonna take in a tasks prop which is a array
+    components: {
+        Task,
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
